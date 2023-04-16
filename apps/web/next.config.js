fc907b02d57185b8cbe0e1651e3f2ecc110fa6ab
Bart Stefanski <contact@bstefanski.com>
2023-04-16 19:04:24 +0200

    chore: setup bundle analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+const withPlugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
+module.exports = withPlugins([[withBundleAnalyzer]], { reactStrictMode: true, transpilePackages: ["ui"] })
