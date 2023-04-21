7249c197ec93be30c78982f51e497dd226eebeff
Bart Stefanski <contact@bstefanski.com>
2023-04-21 18:00:29 +0200

    chore: adjust LP copy

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test"
 test("has title", async ({ page }) => {
   await page.goto("./")
 
-  await expect(page).toHaveTitle(/Hello world/)
+  await expect(page).toHaveTitle(/Next.js Enterprise Boilerplate/)
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Web() {
   return (
     <>
       <Head>
-        <title>Hello world</title>
+        <title>Next.js Enterprise Boilerplate</title>
       </Head>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
@@ -36,7 +36,7 @@ export default function Web() {
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
+                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
                   {singleItem.icon}
                 </div>
                 <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
