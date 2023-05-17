c606968a7801e8304061932e95be0246abca1729
Bart Stefanski <contact@bstefanski.com>
2023-05-17 17:45:25 +0200

    fix: ignore e2e test in jest suite

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,6 +9,7 @@ const config = {
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "jest-environment-jsdom",
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  testPathIgnorePatterns: ["<rootDir>/e2e"],
 }
 
 export default createJestConfig(config)
