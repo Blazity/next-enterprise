df798ea4ccd97094cc570060afc721602e059514
add0215 <mingeon@acon3d.com>
2023-05-30 17:42:29 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react"
 import "../styles/tailwind.css"
 
 import { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    console.log('hello!');
+  }, []);
+
   return <Component {...pageProps} />
 }
 
