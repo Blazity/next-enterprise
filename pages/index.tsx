2c8bb1eb2100e41eed02eadb91e8579cd12c65cb
Bart Stefanski <contact@bstefanski.com>
2023-05-31 17:10:11 +0200

    chore: add og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,10 @@ export default function Web() {
     <>
       <Head>
         <meta property="og:url" content="https://next-enterprise.vercel.app/" />
-        <meta property="og:image" content="https://github.com/Blazity/next-enterprise/blob/main/project-logo.png" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png"
+        />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
