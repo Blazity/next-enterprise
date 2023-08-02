f4f9e47997ff4f5736b2385077c60fea2f2e0541
isaconantonis <sam.antonis@isacon.be>
2023-08-02 12:37:04 +0200

    :

--- providers/NextUiProvider.tsx
+++ providers/NextUiProvider.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { NextUIProvider } from "@nextui-org/react"
 
-export default function NextUiProvider({ children }) {
+export default function NextUiProvider({ children }: { children: React.ReactNode }) {
   return <NextUIProvider>{children}</NextUIProvider>
 }
