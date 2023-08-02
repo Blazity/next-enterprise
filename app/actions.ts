6ba28a795c6c6326d61eb84341db045db979fdee
isaconantonis <sam.antonis@isacon.be>
2023-08-02 11:13:50 +0200

    fix

--- app/actions.ts
+++ app/actions.ts
@@ -1,4 +1,4 @@
-import client from "@/lib/mongodb";
+import client from "../lib/mongodb";
 
 export async function getTeamPlayers() {
     const db = client.db("SloppyFuckers")
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb"
 
-const client = new MongoClient(process.env.MONGODB_URI, {
+const client = new MongoClient(process.env.MONGODB_URI || '', {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
