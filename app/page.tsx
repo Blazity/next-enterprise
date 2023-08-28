c027c645dbbd6c1940d6cf2c868df7f630b6bba4
isaconantonis <sam.antonis@isacon.be>
2023-08-28 15:53:31 +0200

    new graphic

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Web() {
             <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
               <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
                 <div className="mx-auto max-w-2xl lg:mx-0">
-                  <h1 className="mt-4 lg:mt-24 text-4xl font-bold tracking-tight text-primary-800 sm:mt-10 sm:text-6xl">
+                  <h1 className="mt-4 text-4xl font-bold tracking-tight text-primary-800 sm:mt-10 sm:text-6xl lg:mt-24">
                     Your personal <span className="block xl:inline">web development team</span>
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-primary-800">
@@ -26,7 +26,7 @@ export default async function Web() {
               <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
                 <img
                   className="aspect-[3/2] w-full object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
-                  src="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
+                  src="/presentation.svg"
                   alt=""
                 />
               </div>
