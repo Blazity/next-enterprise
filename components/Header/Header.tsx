a0c7c10f22c39a389b41a622ba13ba4b0fde99fd
Nathan <nathancowley12@gmail.com>
2023-12-17 12:17:44 -0700

    Setting up basic skeleton.  Initial commit.

---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Header Component</h1>
+    </header>
+  )
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "components/Button/Button"
+import { Header } from "components/Header/Header"
+
+export default function Web() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-lg font-bold">Hello World</h1>
+    </div>
+  )
+}
