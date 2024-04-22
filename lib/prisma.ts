c9ff0b9e08e420cbe909f67c3d3d597a7c0dae98
Dex404 <x@keshawa.me>
2024-04-22 05:24:55 +0000

    feat: ContactUs Nodemailer

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@/prisma/client"
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+// declare global {
+//   var prisma: PrismaClient | undefined
+// }
 
-export const prisma = globalThis.prisma || new PrismaClient()
+// export const prisma = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
