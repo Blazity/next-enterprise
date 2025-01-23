ef08a7b7818181ccab31a5fdaed8c33749a3aeee
Igor Klepacki <neg4n@icloud.com>
2025-01-23 14:27:03 +0100

    refactor: update next.config to typescript

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import withBundleAnalyzer from "@next/bundle-analyzer"
+import { type NextConfig } from "next"
+
+import { env } from "./env.mjs"
+
+const config: NextConfig = {
+  reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  rewrites: async () => [
+    { source: "/healthz", destination: "/api/health" },
+    { source: "/api/healthz", destination: "/api/health" },
+    { source: "/health", destination: "/api/health" },
+    { source: "/ping", destination: "/api/health" },
+  ],
+}
+
+export default env.ANALYZE ? withBundleAnalyzer({ enabled: env.ANALYZE })(config) : config
