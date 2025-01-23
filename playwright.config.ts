54382d3e3eaf783c6aaef8d0aa3b0772c5b85da8
Igor Klepacki <neg4n@icloud.com>
2025-01-23 17:04:02 +0100

    chore: update bundle and test actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn dev",
+    command: "pnpm dev",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
