c8d0f30e798bd76bb8a2e03212abca94f19ed835
Amir Ali <amiraliazimloo123@gmail.com>
2025-05-30 13:16:23 +0330

    test: add mock for ResizeObserver to support unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.ResizeObserver = ResizeObserverMock;
