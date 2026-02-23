b84825127a1e0cdcaf61c894b38e4cf5e864132b
rushabhdabhade-cmd <rushabh.dabhade@wednesday.is>
2026-02-23 18:06:50 +0530

    basic player ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "styles/tailwind.css"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full">
-      <body className="h-full overflow-hidden bg-zinc-950">{children}</body>
+      <body className="h-full overflow-hidden bg-[#dddce8]">{children}</body>
     </html>
   )
 }
